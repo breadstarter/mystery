c988ce498fe95bc414fa48092b1a3d3ba4e50742
breadstarter <88773300+breadstarter@users.noreply.github.com>
2021-08-18 10:54:28 +0000

    20210818

---
+++ script.js
@@ -0,0 +1,64 @@
+function laskutehtava(ekaArvo, tokaArvo){
+  return ekaArvo + tokaArvo;
+}
+
+function pvmFin(vuosi, kk, pv){
+ let pvm = 2021/08/21;
+pvm.split+("/")[0], [1], [2];
+
+
+}
+
+console.log("täällä ollaan!");
+
+
+let ika = 44;  // numerot kirjoitetaan iman lainau merkkiä
+ika = ika + 1; //integer (int)
+
+let ikakirjaimina = "44"; //string (str)
+ikakirjaimina = ikakirjaimina + 1;
+
+console.log(ikakirjaimina);
+
+console.log("sinun ikasi on" + ika);
+
+let juuvaiei = true; // boolean
+
+console.log("boolean: " + juuvaiei);
+
+let nimi = "Iina Makkonen";
+
+console.log("kirjoita isolla: " + nimi.toUpperCase());
+
+console.log("splittaustaharjoitus: " + nimi.split(' ')[0]);
+
+// while ja for loop:
+
+for(let i=0; i<=10; i++){
+  console.log("loopin iteraatioliki on: " + i);
+}
+
+if(ika > 18 || nimi == "Iina Makkonen"){
+  console.log("onnea olet täysi-ikäinen! ja/tai olet Iina!");
+}
+else if (ika === 18 && nimi == "keijo"){
+  console.log("onnea keijo, olet juuri ja juuri täysi-ikäinen!");
+}
+else{
+  console.error("kasva vielä vähän...");
+}
+
+console.log(laskutehtava(394857, 23897));
+
+
+
+
+
+
+
+
+/* 
+monen 
+rivin 
+kommenti
+*/
