aca9caa6f1dc1f99c9331d2d0504bb49cf9055ae
breadstarter <88773300+breadstarter@users.noreply.github.com>
2021-08-19 10:25:28 +0000

    20210819

--- script.js
+++ script.js
@@ -7,11 +7,23 @@ let pvm="2021/08/21";
 
 // funktio:
 function pvmFin(vuosi, kk, pv){
-  return pv+"."+kk+"."+vuosi;
-
+  if (kk==03 || kk==04 || kk== 05){
+    return "kevät "+pv+"."+kk+"."+vuosi;
+  }
+  else if(kk==06 || kk==07 || kk==8){
+    return "kesä "+pv+"."+kk+"."+vuosi;
+  }
+  else if(kk==09 || kk==10 || kk==11){
+    return "syksy "+pv+"."+kk+"."+vuosi;
+  }
+  else { 
+    return "talvi "+pv+"."+kk+"."+vuosi;
+  }
 }
 
-console.log(pvmFin(pvm.split("/")[0], pvm.split("/")[1], pvm.split("/")[2]));
+
+for(let i=1; i<=15; i++)
+console.log(i+ " " + pvmFin(pvm.split("/")[0], pvm.split("/")[1], pvm.split("/")[2]));
 
 
 console.log("täällä ollaan!");
